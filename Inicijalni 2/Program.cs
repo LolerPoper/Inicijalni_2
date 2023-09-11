bc1f05c42cef4aab295ae787eab5300282f20168
LolerPoper <113007900+LolerPoper@users.noreply.github.com>
2023-09-11 10:39:52 +0200

    Add files via upload

---
+++ Inicijalni 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inicijalni_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+
+            Console.WriteLine("Unesite red znakova: ");
+            a = Console.ReadLine();
+
+            Console.WriteLine(a.Replace(" ", "_"));
+
+            Console.ReadKey();
+        }
+    }
+}
